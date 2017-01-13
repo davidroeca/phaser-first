c078ae1fff1b0c64c5a7777a55e2e3c9bbf22146
davidroeca <david.roeca@gmail.com>
2017-01-13 14:00:12 -0500

    refactor into multiple files and fix global variable issues

---
+++ app/constants.js
@@ -0,0 +1,30 @@
+// Game key
+export const MAIN_KEY = 'main'
+
+// Sprite/image keys
+export const PLAYER = 'player'
+export const ROLLER = 'roller'
+export const SLIME = 'slime'
+
+export const COIN = 'coin'
+export const LAVA = 'lava'
+export const WALL = 'wall'
+
+// Animation key
+export const DEFAULT = 'default'
+
+// Sprite/image Collections
+export const SPRITE_IMAGES = [
+  PLAYER,
+  ROLLER,
+  SLIME
+]
+
+export const GAME_IMAGES = [
+  COIN,
+  LAVA,
+  WALL
+]
+
+// Physics Constants
+export const GRAVITY = 800
